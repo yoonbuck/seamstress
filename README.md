1c20889a3de3c28e118a7d1f3d3c2023272fd0b3
Jordan Yoon-Buck <yoonbuck@uw.edu>
2020-09-22 19:19:25 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,290 @@
+# 🧵 seamstress
+
+✨ easy peasy SPA-ification ✨
+
+seamstress turns your boring, old-fashioned website into a shiny,
+new-fashioned single-page website through the power of javascript 🤖
+
+# a warning
+
+seamstress is new and I am a fool. it is probably not very good and also almost
+certainly has all sorts of security holes. the api will also probably change.
+
+basically, probably don't use this.
+
+# how to
+
+Drop seamstress on every page on your site. seamstress will automatically
+capture clicks on links to the same site, load the new page, and swap out the content.
+
+```html
+<script src="//path/to/seamstress.js"></script>
+```
+
+By default, seamstress will swap out your pages at the node with the
+id `container`. So, you'll want to have pages that are structured like this.
+
+```html
+<!DOCTYPE html>
+<html>
+  <head>
+    ...
+  </head>
+  <body>
+    <!-- global stuff - nav, whatever. won't be replaced by seamstress -->
+    <header>
+      ...
+    </header>
+
+    <!-- seamstress will replace this entire node -->
+    <div id="container">
+      <h1>My super cool page</h1>
+      ...
+    </div>
+
+    <script src="//path/to/seamstress.js"></script>
+  </body>
+</html>
+```
+
+If you want to replace everything, you can probably put this on `<body>` or maybe even
+`<html>`. I haven't tried doing that. It might also fail spectacularly.
+
+If you're already using a different id for your main content element, you can
+tell seamstress to look for that.
+
+```html
+<div id="content">
+  <!-- stuff to replace -->
+</div>
+
+<script src="//path/to/seamstress.js"></script>
+<script>
+  seamstress.setConfig({ target: "content" });
+</script>
+```
+
+Right now, seamstress won't automatically update the `<title>` of the page for you,
+but it probably should. I'll add that soon.
+
+## exceptions
+
+seamstress will always ignore links with the `target` property, so links that open in a new page
+(and any horrifying iframe contraption you've set up) will behave the same. seamstress only works
+on links that are same-origin, so links to content on other domains will behave the same.
+
+If you need seamstress to ignore specific links, you can add the `data-seamstress-ignore` attribute.
+
+If you want to turn off automatic capturing of link clicks entirely, use:
+
+```js
+seamstress.setConfig({ handleLinks: false })
+```
+
+## history
+
+seamstress also handles navigations for history actions (e.g., when the user presses the browser's back button).
+You can also disable this:
+
+```js
+seamstress.setConfig({ handleHistory: false })
+```
+
+seamstress makes no attempt to do anything even remotely related to scroll restoration. Usually what the browser
+does is good enough. If you need something more, it's your problem.
+
+## transitions
+
+seamstress won't do transitions for you, but it allows you to bring your own.
+
+```js
+seamstress.setConfig({
+  transition: async function(oldNode, newNode) {
+    // do something with oldNode and newNode, and return when the transition is over
+  }
+})
+```
+
+## triggering navigation
+
+Sometimes it might be useful to trigger navigation from other scripts, so you can do that.
+
+```js
+seamstress.navigate("/path/to/new/page")
+```
+
+You can pass in additional options in the second parameter, such as a custom transition or event handlers
+for this navigation only:
+
+```js
+seamstress.navigate("/path/to/new/page", {
+  destTarget: "content",
+  transition: async function (newNode, oldNode) {...} ,
+  beforeUnmount: function (newNode, oldNode, options) {...},
+  ...
+})
+```
+
+`seamstress.navigate` will asynchronously return whether the navigation was successful.
+
+```js
+const success = await seamstress.navigate(...)
+```
+
+## running javascript
+
+Javascript you include on the target page won't be run, unless you explicitly
+tell seamstress to do so using `data-seamstress-activate`. I think this works for
+external scripts using `src`, but to be honest, I haven't actually tried that yet.
+
+```html
+<script>
+  console.log("I'll only be run if you load the page directly");
+</script>
+
+<script data-seamstress-activate>
+  console.log("I'll also run when you navigate to this page from another.");
+</script>
+```
+
+You may want to be able to run code when pages are loaded and unloaded.
+You can use `seamstress.onPageMount` and `seamstress.onPageUnmount` for this.
+
+```html
+<script data-seamstress-activate>
+  seamstress.onPageMount(function(newNode, oldNode, options) {
+    // set up event listeners, or whatever
+    // newNode is the node which was just inserted into the page.
+    // oldNode is the one which will soon be removed.
+    // options is the configuration seamstress is using for the ongoing navigation.
+  })
+  seamstress.onPageUnmount(function(newNode, oldNode, options) {
+    // clean up, or whatever
+    // oldNode hasn't been removed from the page yet, but will be right after.
+  })
+</script>
+```
+
+You can pass in asynchronous functions if you want, and seamstress will wait for you,
+but in general it's probably better not to. If you do, you might
+have periods where both old and new content are present on the page at the same time.
+
+seamstress applies the class `seamstress__hidden` to a node which is being mounted
+or unmounted, so you can hide them to prevent this.
+
+```css
+.seamstress__hidden {
+    display: none;
+}
+```
+
+If you want, you can also change the class name used to hide nodes during these overlap periods.
+
+```js
+seamstress.setConfig({ hiddenClass: "custom-class-name" })
+```
+
+By default, the function you pass in to onPageMount will be run immediately if the page just loaded.
+To prevent this and only run the handler if the page is being loaded as part of a seamstress-powered
+navigation, pass in `false` as the second parameter.
+
+```js
+seamstress.onPageMount(function(newNode, oldNode, options) {
+    ...
+}, false)
+```
+
+## events
+
+seamstress provides the ability to listen for a variety of events.
+
+During a navigation, they are dispatched in the order listed here.
+
+#### `beforeNavigate`
+Called before a navigation event occurs. Handlers can cancel the navigation or ask for browser navigation instead.
+
+```js
+seamstress.events.beforeNavigate.add(function(options)) {
+  // options.url contains the url we are navigating to
+  // if this navigation occured because of a link click,
+  //   then options.automatic === "link"
+  // if this navigation occured because of a history event (i.e., user clicked back button)
+  //   then options.automatic === "history"
+  // in either case, the event that caused this is available at options.event.
+  
+  // additionally, options itself is mutable and changes will affect this navigation,
+  // but will not affect future navigations.
+  
+  // return false to cancel navigation.
+  // return seamstress.RELOAD to force a full browser reload.
+})
+```
+
+#### `afterMount`
+Called after the new page's node is added to the document, but before it is visible.
+
+```js
+seamstress.events.afterMount.add(function(newNode, oldNode, options)) {
+  ...
+}
+```
+  
+#### `beforeUnmount`
+Called before the old page's node is removed from the document.
+
+```js
+seamstress.events.beforeUnmount.add(function(newNode, oldNode, options)) {
+  ...
+}
+```
+
+If a `beforeNavigate` handler requested a browser reload, newNode and oldNode will be `null`.
+
+#### `afterNavigate`
+Called after the entire navigation is finished successfully.
+
+```js
+seamstress.events.afterNavigate.add(function(newNode, options)) {
+  ...
+}
+```
+
+### Page events
+
+Calling `.add()` will add an event listener that is called for all future navigations. Use `.remove()` to remove that event listener. To add listeners that only last for the duration of the current page, use `.addPage()`, for example:
+
+```js
+seamstress.events.beforeNavigate.addPage(function(options)) {
+  // this will only be called while still on the current page.
+})
+```
+
+Note that the `.addPage` behavior may be unexpected during a navigation. For example, if a new page uses `beforeUnmount.addPage()` to add own unmount handlers as it is loaded, the handler will actually trigger for the current outgoing page, instead of on the next navigation, as `beforeUnmount` doesn't occur until *after* the new page is loaded and its scripts are run. Use `seamstress.onPageMount` and `seamstress.onPageUnmount` instead as they provide the expected behavior.
+
+### Dispatch order
+
+Listeners for all events are dispatched in the following order:
+
+1. Navigation-only listeners added while calling `navigate()` or added to `options` by a previous listener
+1. Page-only listeners added using `seamstress.events.*.addPage()`
+1. Global listeners added using `seamstress.events.*.add()`
+
+### Async listeners
+
+You can pass asynchronous handlers for any of these events, and the navigation will wait until they are complete, though you should strive to ensure any delay is minimal. See the note above about overlapping content visibility during long navigations.
+
+Async listeners are dispatched in parallel, except for `beforeNavigate`, where listeners are run sequentially in the order listed above. In this case, when any listener returns `false` or `seamstress.RELOAD`, no further listeners are run.
+
+## Error handling
+
+Use the `onError` event to catch errors that occur during navigation.
+
+```js
+seamstress.events.onError.add(function(err)) {
+  // something horrible happened
+}
+```
+
+Improving error handling is ongoing... :)
+
+
---
+++ seamstress.js
@@ -0,0 +1,308 @@
+window.seamstress =
+  window.seamstress ||
+  (function () {
+    "use strict";
+
+    const config = {
+      target: "container",
+      loadSamePage: false,
+      hiddenClass: "seamstress__hidden",
+      handleLinks: true,
+      handleHistory: true,
+    };
+
+    const setProp = (obj, prop, value, writable = false) =>
+      Object.defineProperty(obj, prop, { value, writable, enumerable: true });
+
+    const RELOAD_TOKEN = Symbol();
+    const COMPLETED = Symbol();
+
+    const events = {};
+    const eventFactory = function (resultInterpreter = () => true) {
+      let handlers = [];
+      let pageHandlers = [];
+      return [
+        {
+          /* important: runs async handlers in parallel! */
+          invokeAll(invocationArgs, extraHandlers = []) {
+            return Promise.all(
+              [...extraHandlers, ...pageHandlers, ...handlers].map((f) =>
+                Promise.resolve(f(...invocationArgs))
+              )
+            );
+          },
+          async invoke(invocationArgs, extraHandlers = []) {
+            for (let handler of [
+              ...extraHandlers,
+              ...pageHandlers,
+              ...handlers,
+            ]) {
+              let result = await Promise.resolve(handler(...invocationArgs)); // I think you don't need Promise.resolve here?
+              if (!resultInterpreter(result)) return result;
+            }
+            return COMPLETED;
+          },
+          resetPage() {
+            pageHandlers = [];
+          },
+          removeAll() {
+            pageHandlers = [];
+            handlers = [];
+          },
+        },
+        {
+          add(fn) {
+            handlers.push(fn);
+          },
+          addPage(fn) {
+            pageHandlers.push(fn);
+          },
+          remove(fn) {
+            handlers = handlers.filter((h) => h !== fn);
+            pageHandlers = pageHandlers.filter((h) => h !== fn);
+          },
+        },
+      ];
+    };
+
+    const [beforeNavigateKey, beforeNavigate] = eventFactory(
+      (r) => r === undefined || (r && r !== RELOAD_TOKEN)
+    );
+    setProp(events, "beforeNavigate", beforeNavigate);
+    const [beforeUnmountKey, beforeUnmount] = eventFactory();
+    setProp(events, "beforeUnmount", beforeUnmount);
+
+    // not exposing afterUnmount, but used internally for
+    // onPageUnmount to ensure it can run even if another navigation
+    // occurs immediately
+    const [afterUnmountKey, afterUnmount] = eventFactory();
+    setProp(events, "afterUnmount", afterUnmount);
+
+    const [afterMountKey, afterMount] = eventFactory();
+    setProp(events, "afterMount", afterMount);
+    const [afterNavigateKey, afterNavigate] = eventFactory();
+    setProp(events, "afterNavigate", afterNavigate);
+    const [onErrorKey, onError] = eventFactory();
+    setProp(events, "onError", onError);
+
+    const evtwrap = (h) => (h ? (Array.isArray(h) ? h : [h]) : []);
+
+    let currentPage = location.pathname;
+    let inNavigation = false;
+    let navigationBlocked = false;
+    let navigationTarget;
+    let hasNavigated = false;
+
+    const navigate = async function (url, options) {
+      // skip navigation if it's the page we're already on.
+      // might not catch everything if
+      let opts = Object.assign({ url }, config, options);
+
+      if (url === currentPage && !config.loadSamePage) return false;
+      if (inNavigation && navigationTarget === url) return false;
+      if (navigationBlocked) return false;
+
+      inNavigation = true;
+      navigationTarget = url;
+
+      // run beforeNavigate handlers
+      let result = await beforeNavigateKey.invoke(
+        [opts],
+        evtwrap(opts.beforeNavigate)
+      );
+
+      if (navigationTarget !== url) return; // exit early if possible
+
+      if (result === RELOAD_TOKEN) {
+        // some handler requested hard reload - invoke beforeUnmount
+        await beforeUnmountKey.invokeAll(
+          [null, null, opts],
+          evtwrap(opts.beforeUnmount)
+        );
+        // engage!
+        location.href = url;
+        return; // doesn't really matter
+      } else if (result !== COMPLETED) {
+        // navigation cancelled by some handler
+        inNavigation = false;
+        return false;
+      }
+
+      const $oldNode = document.getElementById(opts.target);
+      if (!$oldNode) {
+        // todo: throw an error instead
+        inNavigation = false;
+        return false; // cannot replace old content
+      }
+
+      if (navigationTarget !== url) {
+        inNavigation = false;
+        return false; // exit early if possible
+      }
+
+      hasNavigated = true;
+      let $newNode, $scripts;
+      try {
+        const response = await fetch(opts.url);
+        if (navigationTarget !== url) {
+          inNavigation = false;
+          return;
+        } // exit early if possible
+        const responseText = await response.text();
+        if (navigationTarget !== url) {
+          inNavigation = false;
+          return;
+        } // exit early if possible
+        const $page = new DOMParser().parseFromString(
+          responseText,
+          "text/html"
+        );
+        $newNode = $page.getElementById(opts.destTarget || opts.target);
+        if ($newNode === null) {
+          throw new Error("target node not found on destination page");
+        }
+        $scripts = $page.querySelectorAll("script[data-seamstress-activate]");
+      } catch (e) {
+        if (navigationTarget !== url) {
+          inNavigation = false;
+          return false;
+        }
+        onErrorKey.invokeAll([e]);
+        inNavigation = false;
+        return false;
+      }
+
+      // this navigation is happening!
+      beforeNavigateKey.resetPage();
+
+      // block new navigations during transition
+      navigationBlocked = true;
+      try {
+        // mount new page
+        $newNode.classList.add(opts.hiddenClass);
+        $oldNode.parentNode.insertBefore($newNode, $oldNode.nextSibling);
+
+        if (options.automatic !== "history") {
+          history.pushState({}, "", url);
+        }
+
+        currentPage = url;
+
+        // run js on new page
+        for (let $script of $scripts) {
+          let $sc = document.createElement("script");
+          $sc.textContent = $script.textContent;
+          if ($script.src) $sc.src = $script.src; // does this work?
+          document.body.appendChild($sc);
+        }
+
+        // run afterMount handler
+        await afterMountKey.invokeAll(
+          [$newNode, $oldNode, opts],
+          evtwrap(opts.afterMount)
+        );
+        afterMountKey.resetPage();
+
+        // run transition
+        $newNode.classList.remove(opts.hiddenClass);
+        if (opts.transition) await opts.transition($newNode, $oldNode);
+        $oldNode.classList.add(opts.hiddenClass);
+
+        // run beforeUnmount handler
+        await beforeUnmountKey.invokeAll(
+          [$newNode, $oldNode, opts],
+          evtwrap(opts.beforeUnmount)
+        );
+        beforeUnmountKey.resetPage();
+
+        // allows a new page to set beforeUnmount handlers asap
+        // using onPageUnmount
+        await afterUnmountKey.invokeAll([opts], []);
+        afterUnmountKey.resetPage();
+
+        $oldNode.parentNode.removeChild($oldNode);
+
+        navigationBlocked = false;
+      } catch (e) {
+        if ($oldNode.parentNode === $newNode.parentNode) {
+          $oldNode.parentNode.removeChild($oldNode);
+          $newNode.classList.remove(opts.hiddenClass);
+        }
+
+        onErrorKey.invokeAll([e]);
+        inNavigation = false;
+        navigationBlocked = false;
+        return false;
+      }
+
+      await afterNavigateKey.invokeAll(
+        [$newNode, opts],
+        evtwrap(opts.afterNavigate)
+      );
+      afterNavigateKey.resetPage();
+
+      inNavigation = false;
+      return true;
+    };
+
+    document.addEventListener("click", (e) => {
+      if (!config.handleLinks) return;
+
+      let $target = e.target;
+      while (
+        $target &&
+        $target !== document.body &&
+        !($target.tagName === "A" && $target.href)
+      ) {
+        $target = $target.parentNode;
+      }
+      if (!$target) return;
+      if ($target.tagName !== "A") return;
+      if ($target.origin !== location.origin) return; // apply to same-origin links only
+      if ($target.target) return; // any target attribute: not for us
+      if ("seamstressIgnore" in $target.dataset) return;
+
+      navigate($target.pathname, {
+        automatic: "link",
+        event: e,
+      });
+      e.preventDefault();
+    });
+
+    window.addEventListener("popstate", (e) => {
+      if (config.handleHistory && location.pathname !== currentPage) {
+        navigate(location.pathname, {
+          automatic: "history",
+          event: e,
+        });
+      }
+    });
+
+    const ifLoading = (fn) => inNavigation && fn();
+
+    const setConfig = (newConfig) => Object.assign(config, newConfig);
+
+    const getConfig = () => ({ ...config });
+
+    const root = {};
+    setProp(root, "events", events);
+    setProp(root, "RELOAD", RELOAD_TOKEN);
+    setProp(root, "navigate", navigate);
+    setProp(root, "setConfig", setConfig);
+    setProp(root, "getConfig", getConfig);
+    setProp(root, "ifLoading", ifLoading);
+
+    setProp(root, "onPageMount", (h, immediate = true) =>
+      hasNavigated
+        ? afterMount.addPage(h)
+        : immediate &&
+          h(document.getElementById(config.target), null, getConfig())
+    );
+    setProp(root, "onPageUnmount", (h) =>
+      hasNavigated
+        ? afterUnmount.addPage(() => beforeUnmount.addPage(h))
+        : beforeUnmount.addPage(h)
+    );
+
+    return root;
+  })();
